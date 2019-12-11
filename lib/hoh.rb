08b9388886f36c6ef7ccc3e9bd9dbcd775e8cefa
Ruari Day <ruariday@gmail.com>
2019-12-11 13:09:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,12 +38,19 @@ def naming_system
       :sub_category => {
         :label => "Class",
         :sub_category => {
-          :label => "Order"
+          :label => "Order",
+          :sub_category => {
+            :label => "Family",
+            :sub_category => {
+              :label => "Genus"
+            }
+          }
         }
       }
     }
   }
-  
+    
+  }
   # Remember:
   #  Kingdom
   #  Phylum
