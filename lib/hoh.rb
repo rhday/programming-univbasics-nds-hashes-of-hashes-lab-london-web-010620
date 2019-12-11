4bb00fbe2af3664516eaa3f03a229452601de26d
Ruari Day <ruariday@gmail.com>
2019-12-11 13:02:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,7 +36,10 @@ def naming_system
     :sub_category => {
       :label => "Phylum",
       :sub_category => {
-        
+        :label => "Class",
+        :sub_category => {
+          :label => "Order"
+        }
       }
     }
   }
