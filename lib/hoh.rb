570ac3eebae2c5b3e0b4cf9b0e79a89bf829a4b5
Ruari Day <ruariday@gmail.com>
2019-12-11 13:12:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,10 @@ def naming_system
       :sub_category => {
         :label => "Class",
         :sub_category => {
-          :label => "Order"
+          :label => "Order",
+          :sub_category => {
+            :label => "Genus"
+          }
         }
       }
     }
